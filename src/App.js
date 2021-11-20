fc28518b600de73482d97bf1027b1245c51c7e86
Hashim Hossam Mahmoud Mohammed <cse-hashim@users.noreply.github.com>
2021-11-20 06:36:14 +0200

    add route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
 import SearchGrid from './SearchGrid'
-
+import { Route, Link } from 'react-router-dom'
 import BookShelf from './BookShelf'
 import Book from './Book'
 import GridLoader from './GirdLoader'
@@ -90,50 +90,56 @@ class BooksApp extends React.Component {
     { shelf: "read", bookshelf_title: "Read" }]
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <button className="close-search" onClick={() => { this.setState({ showSearchPage: false, query: '', searchBooks: [] }) }}>Close</button>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input
-                  type="text"
-                  placeholder="Search by title or author"
-                  value={this.state.query}
-                  onChange={e => this.updateHandler(e.target.value)}
-                />
-
+        <Route  exact path="/"
+          render={({history}) => (
+            <div className="list-books">
+                          {/* {this.setState({query:'',searchBooks:[]})} */}
+              <div className="list-books-title">
+                <h1>MyReads</h1>
+              </div>
+              <div className="list-books-content">
+                <div>
+                  <BookShelf shelfNames={shelfNames} isShelf={true} books={this.state.shelfBooks} updateHandler={this.shelfBookUpdateHandler} />
+                </div>
+              </div>
+              <div className="open-search">
+                {/* <button onClick={() => this.setState({ showSearchPage: true })}>Add a book</button> */}
+                <Link onClick={()=>{this.setState({query:'',searchBooks:[]});history.push('/')}} to={{pathname:"/search"}} >Add a book</Link>
               </div>
             </div>
-            {/* <SearchGrid books={this.state.searchBooks} query={this.state.query} updateHandler={this.updateHandler} updateSearchBooks={this.searchBookUpdateHandler}/> */}
-            <GridLoader shelfBooks={this.state.shelfBooks} query={this.state.query} update={(e) => this.updateSearchBooks(e)} />
-            <BookShelf isShelf={false} books={this.state.shelfBooks} searchBooks={this.state.searchBooks} updateHandler={this.shelfBookUpdateHandler} />
-          </div>
-        ) : (
-          <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-              <div>
-                {/* <ShelfOrganizer /> */}
-                <BookShelf shelfNames={shelfNames} isShelf={true} books={this.state.shelfBooks} updateHandler={this.shelfBookUpdateHandler}/>
-                {/* <BookShelf isShelf={true} books={this.state.shelfBooks} updateHandler={this.shelfBookUpdateHandler} shelf="wantToRead" bookshelf_title="Want To Read" />
-                <BookShelf isShelf={true} books={this.state.shelfBooks} updateHandler={this.shelfBookUpdateHandler} shelf="read" bookshelf_title="Read" /> */}
+          )}
+        />
+
+        <Route exact path="/search"
+          render={({history}) => (
+            <Fragment>
+              <div className="search-books">
+                <div className="search-books-bar">
+                  {/* <button className="close-search" onClick={() => { this.setState({ showSearchPage: false, query: '', searchBooks: [] }) }}>Close</button> */}
+                  <Link to={{pathname:"/"}} onClick={()=>{history.push('/search')}}>Close</Link>
+
+                  <div className="search-books-input-wrapper">
+                    <input
+                      type="text"
+                      placeholder="Search by title or author"
+                      value={this.state.query}
+                      onChange={e => this.updateHandler(e.target.value)}
+                    />
+
+                  </div>
+                </div>
+                <GridLoader shelfBooks={this.state.shelfBooks} query={this.state.query} update={(e) => this.updateSearchBooks(e)} />
+                <BookShelf isShelf={false} books={this.state.shelfBooks} searchBooks={this.state.searchBooks} updateHandler={this.shelfBookUpdateHandler} />
               </div>
-            </div>
-            <div className="open-search">
-              <button onClick={() => this.setState({ showSearchPage: true })}>Add a book</button>
-            </div>
-          </div>
-        )}
+
+            </Fragment>
+          )}
+        />
+
+
+
+
+
       </div>
     )
   }
